75ec9fa1b9183758beb6c36f91b5312e96b43294
RemY🍊 <65098395+Rawknee-69@users.noreply.github.com>
2025-05-06 23:18:55 +0530

    Delete public directory
    del public

--- public/main.d.ts
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import Redis from 'ioredis';
-export declare const redis: "" | Redis | undefined;
-export default function handler(req: any, res: any): Promise<void>;
--- public/routes/meta/mal.d.ts
+++
@@ -1,3 +0,0 @@
-import { FastifyInstance, RegisterOptions } from 'fastify';
-declare const routes: (fastify: FastifyInstance, options: RegisterOptions) => Promise<void>;
-export default routes;
